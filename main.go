7691af19385e120843a9dd5062ce5968295c045a
shubham kumar tiwari <shbhmtwr19@gmail.com>
2023-01-24 12:18:03 +0530

    Create main.go

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	ticker := time.Tick(time.Second)
+	
+	fmt.Printf("Collecting pods info")
+	for i := 1; i <= 10; i++ {
+		<-ticker
+        fmt.Printf("\rContainer status :  %d/10", i) // escape sequence is different in this environment
+	}
+	fmt.Println("\nAll pods up and running.")
+}
