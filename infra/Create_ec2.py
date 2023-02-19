020ec74a460e1a380e225c2dc1e06eaf14e4df76
shubham kumar tiwari <shbhmtwr19@gmail.com>
2023-02-19 14:05:36 +0530

    Update Create_ec2.py

--- infra/Create_ec2.py
+++ infra/Create_ec2.py
@@ -11,13 +11,12 @@ return the public IP address of ec2 created
 
 import boto3
 
-ec2 = boto3.resource('ec2')
+client = boto3.client('ec2')
 
-instances = ec2.create_instances(
-        ImageId="ami-0ff2cb586a7231532",
-        MinCount=1,
-        MaxCount=1,
-        InstanceType="t2.micro",
-        KeyName="ec2-jenkins"
-    )
+instance = ec2.create_instances(
+                ImageId='ami-0ff2cb586a7231532',
+                MaxCount=1,
+                MinCount=1,
+                KeyName='ec2-jenkins'
+                )
      
