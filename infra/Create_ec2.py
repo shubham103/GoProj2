1c322d0df18230e9afc5b7652af75f8318a15022
shubham kumar tiwari <shbhmtwr19@gmail.com>
2023-02-19 14:07:42 +0530

    Update Create_ec2.py

--- infra/Create_ec2.py
+++ infra/Create_ec2.py
@@ -11,7 +11,7 @@ return the public IP address of ec2 created
 
 import boto3
 
-client = boto3.client('ec2')
+client = boto3.resource('ec2')
 
 instance = client.create_instances(
                 ImageId='ami-0ff2cb586a7231532',
