d195ed3dd92f524b989d30e7d2d76af2803fb9f5
shubham kumar tiwari <shbhmtwr19@gmail.com>
2023-02-19 14:23:33 +0530

    Update Create_ec2.py

--- infra/Create_ec2.py
+++ infra/Create_ec2.py
@@ -23,7 +23,7 @@ instance = client.create_instances(
 '''
 import boto3
 
-ec2 = boto3.resource('ec2', region_name='us-east-1', aws_access_key_id='AKIAQJO7ZSYXRPIMM2W4', aws_secret_access_key='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')
+ec2 = boto3.resource('ec2', region_name='us-west-2', aws_access_key_id='AKIAQJO7ZSYXRPIMM2W4', aws_secret_access_key='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')
 
 instance = ec2.create_instances(
     ImageId='ami-0ff2cb586a7231532',
