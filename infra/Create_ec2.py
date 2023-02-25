11ce97eef34883de876d09f4469d3944aa66ba76
shubham kumar tiwari <shbhmtwr19@gmail.com>
2023-02-25 18:00:33 +0530

    Update Create_ec2.py

--- infra/Create_ec2.py
+++ infra/Create_ec2.py
@@ -6,15 +6,29 @@ This file will create a ec2 instance from an existing AMI
 -- wait until the instance is ready
 return the public IP address of ec2 created
 '''
-
 import boto3 as bt
 import time
 import os
 import sys
+import requests
+import base64
 
-ec2 = bt.resource('ec2', region_name='us-east-1', aws_access_key_id=sys.argv[2], aws_secret_access_key=sys.argv[3])
+
+
+ec2 = bt.resource('ec2', region_name='us-east-1', aws_access_key_id='AKIA3TRJZGPS266EZ7US', aws_secret_access_key='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')
+
+client = bt.client('ec2', region_name='us-east-1', aws_access_key_id='AKIA3TRJZGPS266EZ7US', aws_secret_access_key='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')
+
+
+#ec2 = bt.resource('ec2', region_name='us-east-1', aws_access_key_id=sys.argv[2], aws_secret_access_key=sys.argv[3])
+#client = bt.client('ec2',  region_name='us-east-1', aws_access_key_id=sys.argv[2], aws_secret_access_key=sys.argv[3])
+
+
+
 def create():
-    instances = ec2.create_instances(
+
+    instances = ec2.create_instances(ImageId='ami-01fa04d1f425b3c15',InstanceType='t2.micro',KeyName='jenkins-gitlab',MinCount=1,MaxCount=1, SecurityGroupIds=['sg-020cd77a656cf2625',])
+    '''instances = ec2.create_instances(
         ImageId=sys.argv[1],
         InstanceType='t2.micro',
         KeyName='jenkins-gitlab',
@@ -22,23 +36,42 @@ def create():
         MaxCount=1,
         SecurityGroupIds=[sys.argv[4],],
         )
+    '''
     print(instances)
 
-client = bt.client('ec2',  region_name='us-east-1', aws_access_key_id=sys.argv[2], aws_secret_access_key=sys.argv[3])
-def getAll():
+def find_ip():
+
     instances = client.describe_instances()
     for inst in instances['Reservations']:
         if len(inst['Instances'][0]['NetworkInterfaces']) != 0:
+            #print(inst['Instances'][0]['NetworkInterfaces'][0].keys())
+            while True:
+                print("checking association")
+                time.sleep(3)
+                if 'Association' in inst['Instances'][0]['NetworkInterfaces'][0].keys():
+                    print("association found")
+                    break
+        
             public_ip = inst['Instances'][0]['NetworkInterfaces'][0]['Association']['PublicIp']
-            print(f"public_ip={public_ip}")
             return public_ip
-            
 
 create()
+time.sleep(6)
+public_ip = find_ip()
+jenkins_url = f"http://{public_ip}:8080"
+username = "shubham"
+api_token = "11d4713c7830dcab9c483c7be03eab78b0"
 
-public_ip = getAll()
+auth_string = base64.b64encode(f"{username}:{api_token}".encode()).decode()
+
+headers = {
+    "Authorization": f"Basic {auth_string}"
+}
 
 while True:
-    
-
-
+    print("connecting url")
+    response = requests.get(jenkins_url, headers=headers)
+    print("fetching response url", response.status_code)
+    if response.status_code == 200:
+        print("Jenkins server is available!")
+        break
